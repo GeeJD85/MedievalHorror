623192c119520ab284bc2df9c8c81f8f365ebdb9
Graham Wells <gee85@hotmail.co.uk>
2025-01-27 19:07:09 +0000

    New Initial

---
+++ Source/HorrorGame/Private/Character/HorrorCharacter.cpp
@@ -0,0 +1,29 @@
+// Copyright Mintcake Games 2025
+
+
+#include "Character/HorrorCharacter.h"
+
+AHorrorCharacter::AHorrorCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AHorrorCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHorrorCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AHorrorCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
